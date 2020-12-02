3fd0243fe68962791d77490751be6a39e5e06679
João Victor Pacheco <joaovictorspzo@gmail.com>
2020-12-01 22:58:45 -0300

    Aula 2 adicionada

---
+++ Personagem.cs
@@ -0,0 +1,21 @@
+namespace Aula2
+{
+    public class Personagem
+    {
+        public string nome = "Tony Stark";
+        
+        public int idade = 40;
+
+        public string armadura;
+
+
+        public string Atacar(){
+            return "Ataque de fogo!";
+            
+        }
+        public string Defender(){
+            return "Não foi dessa vez!";
+        }
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "Tsukamoto";
+            objetoPersonagem1.idade = 32;
+            objetoPersonagem1.armadura = "Voadora";
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "João";
+            objetoPersonagem2.idade = 18;
+            objetoPersonagem2.armadura = "De Guerra";
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem.Defender()}");
+        }
+
+    }
+}
